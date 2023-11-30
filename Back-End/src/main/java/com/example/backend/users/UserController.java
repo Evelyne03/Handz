b2fb55af430ea3f6ff89adff0dcf0e17830d9a85
toskatron <dusan.teicu02@e-uvt.ro>
2023-11-30 17:27:07 +0200

    update

--- Back-End/src/main/java/com/example/backend/users/UserController.java
+++ Back-End/src/main/java/com/example/backend/users/UserController.java
@@ -1,4 +1,4 @@
-package com.example.handzz.users;
+package com.example.backend.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
