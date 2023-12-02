a44ed9962cbeb1688be712c6b7cc0612352c74ec
Tudureanu Evelyne-Andreea <evelyne.tudureanu03@e-uvt.ro>
2023-12-02 16:15:06 +0200

    Created buttons and rating cards

--- Back-End/src/main/java/com/example/backend/users/UserController.java
+++ Back-End/src/main/java/com/example/backend/users/UserController.java
@@ -1,16 +1,17 @@
-package com.example.handzz.users;
+package com.example.backend.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.backend.users.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path="api")
 public class UserController {
-    private final UserService userService;
+    private final com.example.handzz.users.UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
